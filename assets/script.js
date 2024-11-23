bea53539e73936ef3cb8fca03a194c959b3891e3
invicta7 <vick4dermatology@gmail.com>
2024-11-22 21:40:05 -0800

    interior-designs version 1

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+
+                let menuList =  document.getElementById("menuList")
+                menuList.style.maxHeight = "0px";
+                function toggleMenu(){
+                    if (menuList.style.maxHeight =="0px")
+                    { 
+                        menuList.style.maxHeight = "300px";
+
+                    }
+                    else{
+                        menuList.style.maxHeight = "0px" ;
+                    }
+                }
+        <script src="https://kit.fontawesome.com/c4df35580b.js" crossorigin="anonymous"></script>
